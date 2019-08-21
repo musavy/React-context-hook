e0218769dfc7f84b9bccc6ab91d54404fecc6fc2
musavy <sayed.musavy@integrify.io>
2019-08-21 16:01:58 +0300

    adding style and thems

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemContext';
 class Navbar extends Component {
-	static contextType = ThemeContext;
 	render() {
-		console.log(this.context);
-		const { isLightTheme, light, dark } = this.context;
-		const theme = isLightTheme ? light : dark;
 		return (
-			<nav style={{ background: theme.ui, color: theme.syntax }}>
-				<h1>Context App</h1>
-				<ul>
-					<li>Home</li>
-					<li>About</li>
-					<li>Contact</li>
-				</ul>
-			</nav>
+			<ThemeContext.Consumer>
+				{context => {
+					const { isLightTheme, light, dark } = context;
+					const theme = isLightTheme ? light : dark;
+					return (
+						<nav style={{ background: theme.ui, color: theme.syntax }}>
+							<h1>Context App</h1>
+							<ul>
+								<li>Home</li>
+								<li>About</li>
+								<li>Contact</li>
+							</ul>
+						</nav>
+					);
+				}}
+			</ThemeContext.Consumer>
 		);
 	}
 }
